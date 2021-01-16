858d8f6a13933e1aa1ecbddfad279db96fa95807
BoraA <boraa@d-teknoloji.com.tr>
2021-01-17 01:28:10 +0300

    Added FilePath field to the Task entity.

---
+++ Taskban.WPF/Converters/FilePathToIconConverter.cs
@@ -0,0 +1,46 @@
+﻿namespace Taskban.WPF.Converters
+{
+    using System;
+    using System.Drawing;
+    using System.Globalization;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Interop;
+    using System.Windows.Media.Imaging;
+
+    public class FilePathToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var filePath = (string)value;
+            using var iconForFile = GetIconForFile(filePath);
+
+            return Imaging.CreateBitmapSourceFromHIcon(iconForFile.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+        private static Icon GetIconForFile(string filePath)
+        {
+            var defaultIcon = SystemIcons.WinLogo;
+            var iconForFile = defaultIcon;
+            if (filePath != null && File.Exists(filePath))
+            {
+                try
+                {
+                    iconForFile = Icon.ExtractAssociatedIcon(filePath) ?? defaultIcon;
+                }
+                catch
+                {
+                    iconForFile = defaultIcon;
+                }
+            }
+
+            return iconForFile;
+        }
+    }
+}
--- Taskban.WPF/Entities/Task.cs
+++ Taskban.WPF/Entities/Task.cs
@@ -1,18 +1,80 @@
-﻿using System;
-using System.Collections.ObjectModel;
-
-namespace Taskban.WPF.Entities
+﻿namespace Taskban.WPF.Entities
 {
-    public class Task
+    using System;
+    using System.Collections.ObjectModel;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using Taskban.WPF.Annotations;
+
+    public sealed class Task : INotifyPropertyChanged
     {
+        private string _filePath;
+
+        private string _title;
+
+        private string _description;
+
         public int Id { get; set; }
         public int BoardId { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value == _title)
+                {
+                    return;
+                }
+
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (value == _description)
+                {
+                    return;
+                }
+
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+
         public string Category { get; set; }
         public string Priority { get; set; }
         public DateTime CreatedAt { get; set; }
         public ObservableCollection<Tag> Tags { get; set; }
         public ObservableCollection<SubTask> SubTasks { get; set; }
+
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (value == _filePath)
+                {
+                    return;
+                }
+
+                _filePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
