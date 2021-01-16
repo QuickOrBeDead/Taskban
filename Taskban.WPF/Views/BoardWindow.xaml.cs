1fea575ed85e17c31c5b03e0c6b311551c7cb8d0
BoraA <boraa@d-teknoloji.com.tr>
2021-01-17 01:32:28 +0300

    Added FilePath field to the BoardWindow and implemented add file link and open file link.

--- Taskban.WPF/Views/BoardWindow.xaml.cs
+++ Taskban.WPF/Views/BoardWindow.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using Taskban.WPF.Entities;
-using Taskban.WPF.ViewModels;
-using Syncfusion.UI.Xaml.Kanban;
-
-namespace Taskban.WPF.Views
+﻿namespace Taskban.WPF.Views
 {
+    using System;
+    using System.Diagnostics;
+    using System.Windows;
+
+    using Microsoft.Win32;
+
+    using Syncfusion.UI.Xaml.Kanban;
+
+    using Taskban.WPF.Entities;
+    using Taskban.WPF.ViewModels;
+
     public partial class BoardWindow
     {
         public BoardWindow()
@@ -24,24 +30,62 @@ namespace Taskban.WPF.Views
         public void ClearSubTaskEntry()
         {
             SubTaskIsCompletedCheckBox.IsChecked = false;
-            SubTaskTitleTextBox.Text = "";
+            SubTaskTitleTextBox.Text = string.Empty;
         }
 
         public void ClearTaskTagEntry()
         {
-            TaskTagTextBox.Text = "";
+            TaskTagTextBox.Text = string.Empty;
         }
 
         private void BoardWindow_OnClosed(object sender, EventArgs e)
         {
-            var viewModel = (BoardViewModel) DataContext;
+            SaveBoard();
+        }
+
+        private void SaveBoard()
+        {
+            var viewModel = (BoardViewModel)DataContext;
             viewModel.SaveBoard(null);
         }
 
-        private void AboutMenuItem_Click(object sender, System.Windows.RoutedEventArgs e)
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
         {
             var window = new AboutWindow();
             window.ShowDialog();
+        }
+
+        private void AddFileLinkButton_Click(object sender, RoutedEventArgs e)
+        { 
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var viewModel = (BoardViewModel)DataContext;
+                viewModel.SelectedTask.FilePath = openFileDialog.FileName;
+            }
+        }
+
+        private void OpenFileLinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            var control = (FrameworkContentElement)sender;
+            var task = (Task)control.DataContext;
+
+            try
+            {
+                Process.Start(new ProcessStartInfo(task.FilePath)
+                                  {
+                                      UseShellExecute = true
+                                  });
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(this, $"File Open Error!!{Environment.NewLine}{exception.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SfKanban_OnCardDragEnd(object sender, KanbanDragEndEventArgs e)
+        {
+            SaveBoard();
         }
     }
 }
